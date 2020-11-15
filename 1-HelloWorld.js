64dc11bb2ddb003c0ac193468e7f66779780be7b
Gustavo Martins <gusluud@gmail.com>
2020-11-14 22:08:05 -0300

    First commit

---
+++ 1-HelloWorld.js
@@ -0,0 +1,14 @@
+//Dando o  nosso primeiro hello World com JS
+console.log("Hello World");
+
+console.log ("Trabalhando com variáveis");
+
+//constantes não são mutaveis dã
+const idade = 18;
+
+console.log("Minha idade é : " + idade);
+console.log("Em dois anos tinha: " + (idade + 1));
+console.log("Em dois anos terei : " + (idade + 2));
+console.log("a metade da minha idade é  : " + (idade / 2));
+console.log(" O dobro da minha idade é: " + (idade * 2));
+
---
+++ 2-Variables.js
@@ -0,0 +1,10 @@
+console.log("Travalhando com vaiáveis");
+
+//by the way nao declare variaveis assim
+idade = 9;
+Idade = 40;
+console.log(Idade + " é diferente de " + idade + ", isso porque Js é case sensitive");
+
+const age = 30;
+const name = "Jhon";
+let ano = 2020;
