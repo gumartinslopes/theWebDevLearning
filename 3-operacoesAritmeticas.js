4a60475ebb2a2caaf760a3eca8e038cadf608b14
Gustavo Martins <gusluud@gmail.com>
2020-11-14 22:31:38 -0300

    Saving stuff

--- 3-operacoesAritmeticas.js
+++ 3-operacoesAritmeticas.js
@@ -1,4 +1,3 @@
 console.log("Operacoes aritmeticas");
 
 console.log((10 + 8) * 2 + " É diferente de " + (10 + 8 * 2));
-console.log("a");
---
+++ 5-atribuicao.js
@@ -0,0 +1,14 @@
+console.log("Trabalhando com atribuicao");
+let idade = 45;                       
+const primeiroNome = "James";   //consts sao tipos de variaveis por mais que isso seja estranho
+const sobrenome = "Bond";
+
+console.log(primeiroNome,sobrenome);
+console.log(primeiroNome + " " + sobrenome);
+
+console.log(`The name is ${sobrenome} ${primeiroNome} ${sobrenome}`);
+
+const nomeCompleto = primeiroNome + " " + sobrenome;  //operacao de sobrescrita
+console.log(nomeCompleto); 
+
+idade = 27;
