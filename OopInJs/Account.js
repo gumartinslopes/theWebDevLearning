fb462da90fb5c941a6110a965b80d4be1ec8c9fe
Gustavo Martins <gusluud@gmail.com>
2020-12-27 20:26:02 -0300

    Update files

---
+++ OopInJs/Account.js
@@ -0,0 +1,24 @@
+class Account{
+    agency;
+    _balance = 0; 
+    
+    deposit(value){
+        if(value >= 0){
+            this._balance += value;
+            console.log(` \$${value}.00 deposited successfully`);
+        }  
+        else {
+            console.log('Invalid deposit');
+        }
+    }
+    withdraw(value) {
+        if(this._balance >= value){
+            this._balance -= value;
+            console.log(` \$${value}.00 successfully withdrawn}`);
+            return value;
+        }    
+        else{
+            console.log('Insufficient money');
+        }
+    }
+}
